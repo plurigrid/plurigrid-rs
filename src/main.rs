aaddc5521e21d3db6591705d9be82883673d697a
bluenote <janitachalam@gmail.com>
2023-04-20 18:02:54 -0700

    add http handling

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,90 @@
-use std::rc::Rc;
-use yrs::{
-    Doc, Map, Array, Transaction, WriteTxn, Text, types::{TypeRefs}
-};
+use hyper::server::conn::AddrStream;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Client;
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use yrs::updates::decoder::Decode;
+use yrs::updates::encoder::Encode;
+use yrs::{Doc, ReadTxn, StateVector, Text, Transact, Update};
 
-pub struct Plurigrid {
-    doc: Doc,
+// This function handles incoming HTTP requests and applies received updates
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let update = hyper::body::to_bytes(req.into_body()).await.unwrap();
+    let update = Update::decode_v1(&update).unwrap();
+    {
+        let doc = Doc::new();
+        let mut txn = doc.transact_mut();
+        txn.apply_update(update);
+    }
+    Ok(Response::new(Body::from("Update applied")))
 }
 
-impl Plurigrid {
-    pub fn new() -> Self {
-        let mut doc = Doc::new();
-        let mut txn = doc.write();
+// This function sends updates to other nodes via HTTP
+async fn send_update_to_node(node_addr: &str, update: Vec<u8>) {
+    let client = Client::new();
+    let req = Request::post(node_addr)
+        .header("content-type", "application/octet-stream")
+        .body(Body::from(update))
+        .unwrap();
 
-        let metadata = Map::new();
-        let rows = Array::<Array<Cell>>::new();
-
-        txn.set_map(&doc.root, "metadata", metadata);
-        txn.set_array(&doc.root, "rows", rows);
-
-        txn.commit();
-        Plurigrid { doc }
-    }
-
-    pub fn add_row(&mut self, index: usize) {
-        let mut txn = self.doc.write();
-
-        let rows = txn.get_array::<Array<Cell>>(&self.doc.root, "rows");
-        let new_row = Array::<Cell>::new();
-        txn.insert_into_array(&rows, index, &[Rc::new(TypeRefs::Array(new_row))]);
-
-        txn.commit();
-    }
-
-    pub fn add_cell(&mut self, row_index: usize, cell_index: usize, content: &str) {
-        let mut txn = self.doc.write();
-
-        let rows = txn.get_array::<Array<Cell>>(&self.doc.root, "rows");
-        let row = txn.get_array::<Cell>(&rows, row_index);
-        let new_cell = Text::from(content);
-        txn.insert_into_array(&row, cell_index, &[Rc::new(TypeRefs::Text(new_cell))]);
-
-        txn.commit();
-    }
-
-    pub fn update_cell(&mut self, row_index: usize, cell_index: usize, content: &str) {
-        let mut txn = self.doc.write();
-
-        let rows = txn.get_array::<Array<Cell>>(&self.doc.root, "rows");
-        let row = txn.get_array::<Cell>(&rows, row_index);
-        let cell = txn.get_text(&row, cell_index);
-
-        txn.insert_into_text(&cell, 0, content);
-        txn.delete_range_in_text(&cell, content.len()..cell.len());
-
-        txn.commit();
-    }
-
-    pub fn delete_row(&mut self, index: usize) {
-        let mut txn = self.doc.write();
-
-        let rows = txn.get_array::<Array<Cell>>(&self.doc.root, "rows");
-        txn.delete_range_in_array(&rows, index..index + 1);
-
-        txn.commit();
-    }
-
-    pub fn delete_cell(&mut self, row_index: usize, cell_index: usize) {
-        let mut txn = self.doc.write();
-
-        let rows = txn.get_array::<Array<Cell>>(&self.doc.root, "rows");
-        let row = txn.get_array::<Cell>(&rows, row_index);
-        txn.delete_range_in_array(&row, cell_index..cell_index + 1);
-
-        txn.commit();
+    if let Err(e) = client.request(req).await {
+        eprintln!("Error sending update: {}", e);
     }
 }
 
-// Define the Cell type
-pub struct Cell(Text);
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let doc = Doc::new();
+    let text = doc.get_or_insert_text("root");
 
-impl From<&str> for Cell {
-    fn from(s: &str) -> Self {
-        Cell(Text::from(s))
-    }
-}
+    // Update
+    {
+        let mut txn = doc.transact_mut();
+        text.insert(&mut txn, 0, "micro");
+        text.insert(&mut txn, 5, "world");
+    } // transaction is automatically committed when dropped
 
-fn main() {
+    // Run the HTTP server
+    let make_svc = make_service_fn(|socket: &AddrStream| {
+        let remote_addr = socket.remote_addr();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
+                Ok::<_, Infallible>(Response::new(Body::from(format!(
+                    "Received {}!",
+                    remote_addr
+                ))))
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Start the server
+    let server_handle = tokio::spawn(server);
+
+    // Example nodes to send updates to
+    let nodes: Vec<String> = vec![
+        "http://127.0.0.1:3001".to_string(),
+        "http://127.0.0.1:3002".to_string(),
+    ];
+
+    // Generate an update and send it to other nodes
+    {
+        let remote_doc = Doc::new();
+        remote_doc.get_or_insert_text("article");
+        let remote_timestamp = remote_doc.transact().state_vector().encode_v1();
+        // get update with contents not observed by remote_doc
+        let update = doc
+            .transact()
+            .encode_diff_v1(&StateVector::decode_v1(&remote_timestamp).unwrap());
+        // apply update on remote doc
+        for node in nodes {
+            send_update_to_node(&node, update.clone()).await;
+        }
+    };
+
+    // Keep the server running
+    server_handle.await.unwrap();
 }
